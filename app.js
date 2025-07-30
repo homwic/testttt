5068d7c7c74eabc64cce1d8d4286dba09d94f9db
Muhammad Luthfi <luthfimuhammad19999@gmail.com>
2025-07-30 15:14:56 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
